933d2f583109962b83cc139c1a5844f527c6c294
Brues <5278969+brues-code@users.noreply.github.com>
2026-05-19 16:14:37 -0500

    unused GlobalStrings

--- AddOns/!!!WrathClassicAPI/GlobalStrings.lua
+++
@@ -1,6 +0,0 @@
-ITEMS_VARIABLE_QUANTITY = "%d |4Item:Items;";
-ITEMS_EQUIPPED = "%d |4item:items; equipped";
-ITEMS_IN_INVENTORY = "%d |4item:items; in inventory";
-ITEM_SLOTS_IGNORED = "%d |4slot:slots; ignored";
-ITEM_MISSING = "%s missing";
-ITEMS_NOT_IN_INVENTORY = "%d |4item:items; missing";
